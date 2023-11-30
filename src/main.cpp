dae900511081d75d460c07d9e81454e17e738b5a
Fabio Gasser <Fabio.Gasser@htwg-konstanz.de>
2023-11-30 19:44:20 +0100

    Alles zusammengeführt
    Sollte soweit mit dem aktuellen Display funktionieren

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,7 @@
 #include "RevEng_PAJ7620.h"
 
 #include "Nextion.h"
-
-//#include "TimeLib.h"
-//#include "DCF77.h"
+#include "Config.h"
 
 
 
@@ -25,33 +23,27 @@
 #define main_light_high 5
 #define main_light_pin 3
 
-#define DCF_PIN 2           // Connection pin to DCF 77 device
-#define DCF_INTERRUPT 0    // Interrupt number associated with pin
 #define PIN_LED 13
 #define PIN_schalter 7
 
 int helligkeit; //Wird benötigt für die LDR-Messung
-char hmi_input [7]={}; //Es werden 7 char benötigt, da wir 7 Datensätze pro Button übertragen, um diesen zu identifizieren
 bool flanke_Licht_ein = false;
-int modus=0;
-int rot=0;
-int gruen=0;
-int blau=0;
+int modus=0;  //1 Lernen, 2 Relax, 3 Mix, 4 Party, 6 Auto
 int leuchtstaerke=0;
 bool hauptleuchte_an=false;
 bool indirektebeleuchtung_an=false;
 int durchlaufzaehler_party_farbwechsel=0;
 volatile int flankenzaehler_ein_aus=0;
+int activeLamp=0; //0 beide aus; 1 Haupt; 2 Neben; 3 beide
 
 int red,green,blue,bright;
+uint32_t memory;
 
 
 // Funktionen:
 int RGB_Licht_Funktion(int, int, int, int, int, int, bool, bool); //(Pixel,R,G,B,Helligkeit,Modus,Hauptleuchte_an,Indirektebeleuchtung_an)
 int Signalgeber(int, int); //(An, Modus)
 int Gestensensor(); //Gestensensor
-void digitalClockDisplay();  // DCF77
-void printDigits(int);  // DCF77
 int LDR_Messung(); //LDR Messung zwischen 0 und 1023
 void Serielle_Textausgabe(String, String); //Textausgabe zum HMI
 void HMI_Input_loeschen(char*);
@@ -75,26 +67,35 @@ RevEng_PAJ7620 sensor = RevEng_PAJ7620();
 
 
 //Displayelemente
-NexSlider h0 = NexSlider(0, 2, "h0"); //Slider initialisieren rot; Touch-Release Event muss noch konfiguriert werden
-NexSlider h1 = NexSlider(0, 7, "h1"); //Slider gruen
-NexSlider h2 = NexSlider(0, 9, "h2"); //Slider blau
-NexSlider h3 = NexSlider(0, 11, "h3");  //Slider helligkeit
-NexDSButton bt1 = NexDSButton(0, 4, "bt1"); //Button Licht an/aus
-NexCheckbox c0 = NexCheckbox(0, 3, "c0");
+NexSlider h_red_ls = NexSlider(7, 1, "h_red_ls"); //Slider initialisieren rot; Touch-Release Event muss noch konfiguriert werden
+NexSlider h_green_ls = NexSlider(7, 4, "h_green_ls"); //Slider gruen
+NexSlider h_blue_ls = NexSlider(7, 6, "h_blue_ls"); //Slider blau
+NexSlider h_hell_ls = NexSlider(7, 8, "h3");  //Slider helligkeit
+NexDSButton bt_save_ls = NexDSButton(7, 2, "bt_save_ls"); //Button Licht an/aus
+NexButton b_switch_ls = NexButton(7,15,"b_switch_ls");  //Button um die Lampen umzuschalten
+NexDSButton bt_lernen_lk = NexDSButton(1,13,"bt_lernen_lk");
+NexDSButton bt_relax_lk = NexDSButton(1,15,"bt_relax_lk");
+NexDSButton bt_auto_lk = NexDSButton(1,14,"bt_auto_lk");
+NexDSButton bt_party_lk = NexDSButton(1,16,"bt_party_lk");
+NexDSButton bt_mix_lk = NexDSButton(1,3,"bt_mix_lk");
+NexRadio r_hauptle_lk = NexRadio(1,10,"r_hauptle_lk");
+NexRadio r_indirektb_lk = NexRadio(1,11,"r_indirektb_lk");
 
 NexTouch *nex_listen_list[]={
-  &h0,
-  &h1,
-  &h2,
-  &h3,
-  &bt1,
-  &c0
+  &h_red_ls,
+  &h_green_ls,
+  &h_blue_ls,
+  &h_hell_ls,
+  &bt_save_ls,
+  &b_switch_ls,
+  &bt_lernen_lk,
+  &bt_relax_lk,
+  &bt_auto_lk,
+  &bt_party_lk,
+  &bt_mix_lk,
+  &r_hauptle_lk,
+  &r_indirektb_lk
 };
-//DCF77
-//time_t time;
-//DCF77 DCF = DCF77(DCF_PIN, DCF_INTERRUPT);
-// wurde ein gültiges Signal gefunden
-//bool g_bDCFTimeFound = false;
 
 #pragma region DisplayFunctions
 bool sendCmdToDisplay(String command){
@@ -104,39 +105,53 @@ bool sendCmdToDisplay(String command){
   Serial.write(0xFF);
 }
 
+void setColorsActive(){
+  if (activeLamp==2)
+  {
+    main_light.fill(main_light.Color(red, green, blue));
+    main_light.setBrightness(bright);
+    main_light.show();
+  }else /*if (activeLamp==3)*/{
+    strip_IndLi.fill(strip_IndLi.Color(red, green, blue));
+    strip_IndLi.setBrightness(bright);
+    strip_IndLi.show();
+    strip_IndRe.fill(strip_IndRe.Color(red, green, blue));
+    strip_IndRe.setBrightness(bright);
+    strip_IndRe.show();
+  }
+}
+
 /*
 void h0PushCallback(void *ptr)      fuer Touch Press Event*/
 
-void h0PopCallback(void *ptr){
-  uint32_t state=0;
-  h0.getValue(&state);
-  red=state;
-  strip_IndLi.fill(strip_IndLi.Color(red, green, blue));
-    strip_IndLi.setBrightness(bright);
-    strip_IndLi.show();
+void h_red_lsPopCallback(void *ptr){
+  h_red_ls.getValue(&memory);
+  red=memory;
+  setColorsActive();
 }
 
-void h1PopCallback(void *ptr){
-  uint32_t state=0;
-  h1.getValue(&state);
-  green=state;
+void h_green_lsPopCallback(void *ptr){
+  h_green_ls.getValue(&memory);
+  green=memory;
+  setColorsActive();
 }
 
-void h2PopCallback(void *ptr){
-  uint32_t state=0;
-  h2.getValue(&state);
-  blue=state;
+void h_blue_lsPopCallback(void *ptr){
+  h_blue_ls.getValue(&memory);
+  blue=memory;
+  setColorsActive();
 }
 
-void h3PopCallback(void *ptr){
-  uint32_t state=0;
-  h3.getValue(&state);
-  bright=state;
+void h_hell_lsPopCallback(void *ptr){
+  h_hell_ls.getValue(&memory);
+  bright=memory;
+  setColorsActive();
 }
 
-void bt1PopCallback(void *ptr){
+// TODO - Die Farbwerte für den aktuellen Modus speichern
+void bt_save_lsPopCallback(void *ptr){
   uint32_t state=0;
-  bt1.getValue(&state);
+  bt_save_ls.getValue(&state);
 
   if(state == 1){
     strip_IndLi.fill(strip_IndLi.Color(red, green, blue));
@@ -149,18 +164,59 @@ void bt1PopCallback(void *ptr){
 
 }
 
-void c0PopCallback(void *ptr){
-  uint32_t state=0;
-  c0.getValue(&state);
-
-  if(state == 1){
-    RGB_Licht_Funktion(0, 0, 0, 0, 255, 4, true, true);
-  }else{
-    strip_IndLi.fill(strip_IndLi.Color(red, green, blue));
-    strip_IndLi.setBrightness(bright);
-    strip_IndLi.show();
+void b_switch_lsPopCallback(void *ptr){
+  String text;
+  switch(activeLamp){
+    case 0: activeLamp=1;
+       text="Haupt-\rleuchte";
+       break;
+    case 1: activeLamp=2;
+       text="Neben-\rleuchte";
+       break;
+    case 2: activeLamp=3;
+       text="Beide\rLeuchten";
+       break;
+    default: activeLamp=0;
+       text="Beide\raus";
+       break;
   }
+  Serielle_Textausgabe("b_switch_ls.txt=",text);
+  setColorsActive();
+}
 
+void bt_lernen_lkPopCallback(void *ptr){
+  modus=1; //Arbeitslicht (Lernen)
+  Serielle_Textausgabe("t_actmod_lk.txt=","Lernen"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
+}
+
+void bt_relax_lkPopCallback(void *ptr){
+  modus=2; //Entspannungslicht (Relax)
+  Serielle_Textausgabe("t_actmod_lk.txt=","Relax"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
+}
+
+void bt_party_lkPopCallback(void *ptr){
+  modus=4; //Party
+  Serielle_Textausgabe("t_actmod_lk.txt=","Party"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
+}
+
+void bt_auto_lkPopCallback(void *ptr){
+  modus=6; //Lichtabhängige Lichtansteuerung (Automatik)
+  Serielle_Textausgabe("t_actmod_lk.txt=","Automatik"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
+}
+
+void bt_mix_lkPopCallback(void *ptr){
+  modus=3; //Farben mix
+  Serielle_Textausgabe("t_actmod_lk.txt=","Farbenmix"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
+}
+
+void r_hauptle_lkPopCallback(void *ptr){
+  r_hauptle_lk.getValue(&memory);
+  hauptleuchte_an=memory?true:false;
+}
+
+void r_indirektb_lkPopCallback(void *ptr){
+  r_indirektb_lk.getValue(&memory);
+  indirektebeleuchtung_an=memory?true:false;
 }
 #pragma endregion
 
@@ -195,23 +251,20 @@ void setup()
   Serial.println("PAJ7620 init: OK");
   Serial.println("Please input your gestures:"); */
 
-  //DCF77  
- /* pinMode(PIN_LED, OUTPUT);
-  pinMode(PIN_schalter, OUTPUT);
-  digitalWrite(PIN_schalter, LOW);
-  Serial.begin(9600); 
-  DCF.Start();
-  Serial.println("Warten auf DCF77-Zeit... ");
-  Serial.println("Dies dauert mindestens 2 Minuten, in der Regel eher länger.");
-  delay(2000); */
-
   //Display Events zu Artefakten hinzufuegen
-  h0.attachPop(h0PopCallback);
-  h1.attachPop(h1PopCallback);
-  h2.attachPop(h2PopCallback);
-  h3.attachPop(h3PopCallback);
-  bt1.attachPop(bt1PopCallback);
-  c0.attachPop(c0PopCallback);
+  h_red_ls.attachPop(h_red_lsPopCallback);
+  h_green_ls.attachPop(h_green_lsPopCallback);
+  h_blue_ls.attachPop(h_blue_lsPopCallback);
+  h_hell_ls.attachPop(h_hell_lsPopCallback);
+  bt_save_ls.attachPop(bt_save_lsPopCallback);
+  b_switch_ls.attachPop(b_switch_lsPopCallback);
+  bt_lernen_lk.attachPop(bt_lernen_lkPopCallback);
+  bt_relax_lk.attachPop(bt_relax_lkPopCallback);
+  bt_auto_lk.attachPop(bt_auto_lkPopCallback);
+  bt_party_lk.attachPop(bt_party_lkPopCallback);
+  bt_mix_lk.attachPop(bt_mix_lkPopCallback);
+  r_hauptle_lk.attachPop(r_hauptle_lkPopCallback);
+  r_indirektb_lk.attachPop(r_indirektb_lkPopCallback);
 
   //HMI
   String cmd; 
@@ -223,10 +276,6 @@ void setup()
     Serial.write(0xFF);
     Serial.write(0xFF);
   }
-
-  strip_IndLi.fill(strip_IndLi.Color(100, 0, 0));
-  strip_IndLi.setBrightness(150);
-  strip_IndLi.show();
 }
 
 
@@ -235,92 +284,12 @@ void loop()
 {
   nexLoop(nex_listen_list);
 
+  // TODO: Je nach aktivem Modus die Lampen ansteuern?
   //RGB_Licht_Funktion(0, 0, 0, 0, 255, 4);
   //RGB_Licht_Funktion(0, 0, 0, 0, 255, 6);
   //Signalgeber(0,0);
   //Gestensensor();
   //RGB_Licht_Funktion(0, 0, 0, 0, 0, 6);
-
-  /*
-  Serial.println(Serial.available());
-  //HMI
-  if(Serial.available() > 0) //Prüfe ob Serielle Schnittstelle erreichbar
-  {
-    for(int i=0;i<6;i++) //Eingehende Nummer von Inputs einlesen (xx yy zz dd aa uu ii) yy=Seite, zz=Component ID 1,2,3
-    {
-      hmi_input[i]=hmi_input[i+1];
-      Serial.println(hmi_input[i]);
-    }
-    hmi_input[6]=Serial.read();
-  }
-
-  if(hmi_input[1]==0&&hmi_input[0]==65){//Touch Event
-    switch(hmi_input[2]){
-      case 2: //Rot-Wert
-        red=hmi_input[5];
-        sendCmdToDisplay("n0.val="+red);
-        //Weitergabe an die LED-Streifen
-        break;
-      case 7: //Gruen-Wert
-        green=hmi_input[5];
-        sendCmdToDisplay("n1.val="+green);
-        break;
-      case 9: //Blau-Wert
-        blue=hmi_input[5];
-        sendCmdToDisplay("n2.val="+blue);
-        break;
-      case 11: //Helligkeit
-        bright=hmi_input[5];
-        sendCmdToDisplay("n3.val="+bright);
-        break;
-    }
-
-    for(int i=0; i<7;i++) //Inputdatenarray löschen
-    {
-      hmi_input[i]=0;
-    }
-  }
-  */
-
-  /*
-  if(hmi_input[1]==0 && hmi_input[2]==5) //hmi_input[1]=Seite, hmi_input[2]=Button => Ein/Aus
-  { 
-    int flankenerkennung;
-    if(flanke_ein==true)
-    {
-      RGB_Licht_Funktion(0, 0, 0, 0, 0, 1);
-      flanke_ein=false;
-      flankenerkennung = 1;
-      for(int i=0;i<=2;i++){
-      Serial.print("texbox_1.txt=\"Licht Aus\"");
-      Serial.write(0xFF);
-      Serial.write(0xFF);
-      Serial.write(0xFF);
-      }
-    }
-    else if (flanke_ein==false && flankenerkennung==0)
-    {
-      RGB_Licht_Funktion(0, 255, 255, 255, 255, 1);
-      flanke_ein=true;
-      for(int i=0;i<=2;i++){
-      Serial.print("texbox_1.txt=\"Licht Ein\"");
-      Serial.write(0xFF);
-      Serial.write(0xFF);
-      Serial.write(0xFF);
-      }
-      
-    }
-    flankenerkennung = 0;
-        
-    for(int i=0; i<7;i++) //Inputdatenarray löschen
-    {
-      hmi_input[i]=0;
-    }
-  }
-  */
-
-  //RGB_Licht_Funktion(0, 0, 0, 0, 0, 1);
-
 
 /*
   Serial.print("va0.val=42");   //Sending Code to the Display; this case: value of va0 is 42
@@ -336,54 +305,41 @@ void loop()
 
 }
 
+void setModusActive(){
+  switch (modus){
+    case 1: //Lernen
+      red=255;
+      green=255;
+      blue=255;
+      leuchtstaerke=255;
+      break;
+    case 2: //Entspannungslicht (Relax)
+      red=241;
+      green=142;
+      blue=28;
+      leuchtstaerke=255;
+      break;
+    case 4: //Party
+      leuchtstaerke=255;
+      break;
+    case 3: //Farben mix
+      break;
+    case 6: //Lichtabhängige Lichtansteuerung (Automatik)
+      break;
+    default:
+      break;
+  }
+}
+
+
 int LDR_Messung()
 {
   helligkeit = analogRead(0);
   Serial.println(helligkeit);
   delay(500);
   
-  if(hmi_input[1]==1 && (hmi_input[2]==3 || hmi_input[2]==4 || hmi_input[2]==5 || hmi_input[2]==6 || hmi_input[2]==7)) //hmi_input[1]=> Seite 2, hmi_input[2] => Buttons Modies
-  {
-    switch (hmi_input[2])
-    {
-    case 3:
-      modus=1; //Arbeitslicht (Lernen)
-      rot=255;
-      gruen=255;
-      blau=255;
-      leuchtstaerke=255;
-      Serielle_Textausgabe("texbox_1_lk.txt=","Lernen"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
-      break;
-    case 4:
-      modus=2; //Entspannungslicht (Relax)
-      rot=241;
-      gruen=142;
-      blau=28;
-      leuchtstaerke=255;
-      Serielle_Textausgabe("texbox_1_lk.txt=","Relax"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
-      break;
-    case 5:
-      modus=4; //Party
-      leuchtstaerke=255;
-      Serielle_Textausgabe("texbox_1_lk.txt=","Party"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
-      break;
-    case 6:
-      modus=3; //Farben mix
-      Serielle_Textausgabe("texbox_1_lk.txt=","Farbenmix"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
-      break;
-    case 7:
-      modus=6; //Lichtabhängige Lichtansteuerung (Automatik)
-      Serielle_Textausgabe("texbox_1_lk.txt=","Automatik"); //Ausgabetext in Textbox 1 auf der Seite Licht konfiguration (Seite2)
-      break;
-    
-    default:
-      break;
-    }
-  }
 
 /*
-void DCF77()
-{
    // das Signal wird nur aller 5 Sekunden abgefragt
   delay(950);
 
@@ -492,38 +448,9 @@ void DCF77()
   }
  
   
-// das Signal wird nur aller 5 Sekunden abgefragt
-  delay(950);
-  digitalWrite(PIN_LED, HIGH);
-  delay(50);
-  digitalWrite(PIN_LED, LOW);
-  time_t DCFtime = DCF.getTime(); // Check if new DCF77 time is available
-  if (DCFtime!=0)
-  {
-    Serial.println("Time is updated");
-    setTime(DCFtime);
-    g_bDCFTimeFound = true;
-  }
-  
-  // die Uhrzeit wurde gesetzt, also LED nach kurzer Zeit ein
-  if (g_bDCFTimeFound)
-  {
-    delay(50);
-    digitalWrite(PIN_LED, HIGH);
-  }
-  digitalClockDisplay();*/
+*/
 }
 
-
-
-
-void HMI_Input_loeschen(char* HMI_Input_array)
-{
-  for(int i=0; i<7;i++) //Inputdatenarray löschen
-      {
-        HMI_Input_array[i]=0;
-      }
-}
 
 void Serielle_Textausgabe(String textbox, String text)
 {
@@ -542,37 +469,8 @@ int LDR_Messung()
   helligkeit = analogRead(0);  
   return helligkeit;
 }
-
-void DCF77()
-{
-   
-}
-
-
-void digitalClockDisplay()
-{
-  // digital clock display of the time
-  printDigits(hour());
-  Serial.print(":");
-  printDigits(minute());
-  Serial.print(":");
-  printDigits(second());
-  Serial.print(" ");
-  Serial.print(day());
-  Serial.print(" ");
-  Serial.print(month());
-  Serial.print(" ");
-  Serial.print(year()); 
-  Serial.println();
-}
 */
-void printDigits(int digits)
-{
-  // utility function for digital clock display: prints preceding colon and leading 0
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
+
 
 int Gestensensor()
 {
