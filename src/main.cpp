d3d7ab739ab77cc9eac7be34a686336f525d1709
Florian M <137206298+Flo1205@users.noreply.github.com>
2023-12-19 17:48:21 +0100

    Ohne Nextion sachen

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 #include <Arduino.h>
 //#include "avr8-stub.h"
 //#include "app_api.h" //only needed with flash breakpoints
-#include <Adafruit_NeoPixel.h>  //Indirekte Beleuchtung
+#include "Adafruit_NeoPixel.h"  //Indirekte Beleuchtung
 #include <avr/power.h>
 #include <Wire.h> //wird gebraucht für I2C-Kommunikation mit dem Gestensensor
 #include "RevEng_PAJ7620.h"  //Gestensensor
-#include <RTClib.h>  //Realtime-Clock
+#include "RTClib.h"  //Realtime-Clock
 #include "Nextion.h"
 
 
@@ -25,9 +25,7 @@
 #define PIN_schalter 7
 #define PIN_summer 4 //Pin an dem der Summer angeschlossen ist
 const byte PIN_SQW = 2; //Interruptpin RTC -> SQW pin is used to monitor the SQW 1Hz output from the DS3231
-//TODO:Ändern in Boolean
-volatile uint8_t flanke_rtc_sqw; //A variable to store when a falling 1Hz clock edge has occured on the SQW pin of the DS3231
-
+bool flanke_rtc_sqw; //A variable to store when a falling 1Hz clock edge has occured on the SQW pin of the DS3231
 uint8_t helligkeit; //Wird benötigt für die LDR-Messung
 bool flanke_Licht_ein = false;
 uint8_t modus=0;  //1 Lernen, 2 Relax, 3 Mix, 4 Party, 6 Auto
@@ -37,9 +35,8 @@ bool indirektebeleuchtung_an=false;
 int durchlaufzaehler_party_farbwechsel=0;
 volatile uint8_t flankenzaehler_ein_aus=0;
 uint8_t activeLamp=0; //0 beide aus; 1 Haupt; 2 Neben; 3 beide
-
 uint8_t red,green,blue,bright;
-uint32_t memory;
+//uint32_t memory;
 
 
 // Funktionen:
@@ -73,7 +70,7 @@ char wochentage[7][12] = {"So","Mo", "Di", "Mi", "Do", "Fr", "Sa"};
 char monate_des_jahres[12][12] = {"Januar", "Februar", "Maerz", "April", "Mai", "Juni","Juli", "August", "September", "Oktober", "November", "Dezember"};  
 
 //Displayelemente
-NexSlider h_red_ls = NexSlider(7, 1, "h_red_ls"); //Slider initialisieren rot; Touch-Release Event muss noch konfiguriert werden
+/*NexSlider h_red_ls = NexSlider(7, 1, "h_red_ls"); //Slider initialisieren rot; Touch-Release Event muss noch konfiguriert werden
 NexSlider h_green_ls = NexSlider(7, 4, "h_green_ls"); //Slider gruen
 NexSlider h_blue_ls = NexSlider(7, 6, "h_blue_ls"); //Slider blau
 NexSlider h_hell_ls = NexSlider(7, 8, "h3");  //Slider helligkeit
@@ -85,8 +82,9 @@ NexDSButton bt_auto_lk = NexDSButton(1,14,"bt_auto_lk");
 NexDSButton bt_party_lk = NexDSButton(1,16,"bt_party_lk");
 NexDSButton bt_mix_lk = NexDSButton(1,3,"bt_mix_lk");
 NexRadio r_hauptle_lk = NexRadio(1,10,"r_hauptle_lk");
-NexRadio r_indirektb_lk = NexRadio(1,11,"r_indirektb_lk");
+NexRadio r_indirektb_lk = NexRadio(1,11,"r_indirektb_lk");*/
 
+/*
 NexTouch *nex_listen_list[]={
   &h_red_ls,
   &h_green_ls,
@@ -103,7 +101,8 @@ NexTouch *nex_listen_list[]={
   &r_indirektb_lk
 };  
 
-
+*/
+/*
 #pragma region DisplayFunctions
 //TODO: Wird die folgende Funktion gebraucht, weil wir schon so eine Funktion haben??
 bool sendCmdToDisplay(String command){
@@ -119,7 +118,7 @@ void setColorsActive(){
     main_light.fill(main_light.Color(red, green, blue));
     main_light.setBrightness(bright);
     main_light.show();
-  }else /*if (activeLamp==3)*/{
+  }else{
     strip_IndLi.fill(strip_IndLi.Color(red, green, blue));
     strip_IndLi.setBrightness(bright);
     strip_IndLi.show();
@@ -130,7 +129,7 @@ void setColorsActive(){
 }
 
 /*
-void h0PushCallback(void *ptr)      fuer Touch Press Event*/
+void h0PushCallback(void *ptr)      fuer Touch Press Event
 
 void h_red_lsPopCallback(void *ptr){
   h_red_ls.getValue(&memory);
@@ -226,7 +225,7 @@ void r_indirektb_lkPopCallback(void *ptr){
   r_indirektb_lk.getValue(&memory);
   indirektebeleuchtung_an=memory?true:false;
 }
-#pragma endregion
+#pragma endregion  */
 
 
 // Setupmethode, diese Methode beeinhaltet alle Grundeinstellungen z.B. ob ein Kanal ein Eingang oder Ausgang ist. 
@@ -260,7 +259,7 @@ void setup()
   Serial.println("Please input your gestures:"); */
 
   //Display Events zu Artefakten hinzufuegen
-  h_red_ls.attachPop(h_red_lsPopCallback);
+/* h_red_ls.attachPop(h_red_lsPopCallback);
   h_green_ls.attachPop(h_green_lsPopCallback);
   h_blue_ls.attachPop(h_blue_lsPopCallback);
   h_hell_ls.attachPop(h_hell_lsPopCallback);
@@ -273,7 +272,7 @@ void setup()
   bt_mix_lk.attachPop(bt_mix_lkPopCallback);
   r_hauptle_lk.attachPop(r_hauptle_lkPopCallback);
   r_indirektb_lk.attachPop(r_indirektb_lkPopCallback);
-
+*/ 
   //HMI
  /*String cmd; 
   cmd+= "\"";
@@ -310,14 +309,14 @@ void setup()
   pinMode(PIN_SQW, INPUT); //Configure the SQWinput pin as an INPUT to monitor the SQW pin of the DS3231.
   digitalWrite (PIN_SQW, HIGH); //Enable the internal pull-up resistor, since the SQW pin on the DS3231 is an Open Drain output.
   attachInterrupt(digitalPinToInterrupt(PIN_SQW), ISR_RTC, FALLING); //Configure SQWinput (pin 2 of the Arduino) for use by the Interrupt Service Routine (Isr)
-  flanke_rtc_sqw = 1; //Initialize EDGE equal to 1. The Interrupt Service Routine will make EDGE equal to zero when triggered by a falling clock edge on SQW
+  flanke_rtc_sqw = true; //Initialize EDGE equal to 1. The Interrupt Service Routine will make EDGE equal to zero when triggered by a falling clock edge on SQW
 }
 
 
 // Main-Code-Schleife, diese Methode wird ständig wiederholt
 void loop() 
 {
-  nexLoop(nex_listen_list);
+  //nexLoop(nex_listen_list);
 
   // TODO: Je nach aktivem Modus die Lampen ansteuern?
   //RGB_Licht_Funktion(0, 0, 0, 0, 255, 4);
@@ -339,10 +338,10 @@ void loop()
 */
 
 
- if (flanke_rtc_sqw == 0) //Test if EDGE has been made equal to zero by the Interrrupt Service Routine(ISR).  If it has, then update the time displayed on the clock
+ if (flanke_rtc_sqw == false) //Test if EDGE has been made equal to zero by the Interrrupt Service Routine(ISR).  If it has, then update the time displayed on the clock
   {
     displayTime ();
-    flanke_rtc_sqw = 1; // The time will not be updated again until another falling clock edge is detected on the SQWinput pin of the Arduino.
+    flanke_rtc_sqw = true; // The time will not be updated again until another falling clock edge is detected on the SQWinput pin of the Arduino.
   }
 
 
@@ -386,7 +385,7 @@ static uint8_t wochentage_memory=0;
 
 //Interrupt Service Routine - This routine is performed when a falling edge on the 1Hz SQW clock from the RTC is detected
 void ISR_RTC () {
-    flanke_rtc_sqw = 0; //A falling edge was detected on the SQWinput pin.  Now set EDGE equal to 0.
+    flanke_rtc_sqw = false; //A falling edge was detected on the SQWinput pin.  Now set EDGE equal to 0.
 }
 
 void displayTime () {
